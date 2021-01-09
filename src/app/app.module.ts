eb3f53d33b8e2083c2c8b0ef473b28aa759fee50
Sean Blessing <snblessing@gmail.com>
2021-01-09 15:20:44 -0500

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './feature/movie/movie-list/movie-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/feature/movie/movie-list/movie-list.component.ts
@@ -0,0 +1,29 @@
+import { MovieService } from './../../../service/movie.service';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/model/movie.class';
+
+@Component({
+  selector: 'app-movie-list',
+  templateUrl: './movie-list.component.html',
+  styleUrls: ['./movie-list.component.css']
+})
+export class MovieListComponent implements OnInit {
+  title = "Movie List";
+  movies: Movie[] = [];
+
+  constructor(private movieSvc: MovieService) { }
+
+  ngOnInit(): void {
+    // populate list of movies
+    this.movieSvc.getAll().subscribe(
+      resp => {
+        this.movies = resp as Movie[];
+        console.log('Movies',this.movies);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+}
