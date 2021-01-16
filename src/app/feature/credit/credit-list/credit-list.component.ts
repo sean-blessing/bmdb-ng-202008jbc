0ec41fb4dfd066f881b00592d0a8d67c2f69154c
Sean Blessing <snblessing@gmail.com>
2021-01-16 10:53:55 -0500

    Added Credit List, Credit Create components

---
+++ src/app/feature/credit/credit-list/credit-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Credit } from 'src/app/model/credit.class';
+import { CreditService } from 'src/app/service/credit.service';
+
+@Component({
+  selector: 'app-credit-list',
+  templateUrl: './credit-list.component.html',
+  styleUrls: ['./credit-list.component.css']
+})
+export class CreditListComponent implements OnInit {
+  title = "Credit List";
+  credits: Credit[] = [];
+  constructor(private creditSvc: CreditService) { }
+
+  ngOnInit(): void {
+    // populate list of credits
+    this.creditSvc.getAll().subscribe(
+      resp => {
+        this.credits = resp as Credit[];
+        console.log('Credits', this.credits);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+}
