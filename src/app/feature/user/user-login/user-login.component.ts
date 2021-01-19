564da2b8f1cbcc97dccb89c97a38632595ae8418
Sean Blessing <snblessing@gmail.com>
2021-01-18 19:32:28 -0500

    added user login service call

---
+++ src/app/feature/user/user-login/user-login.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/model/user.class';
+import { UserService } from 'src/app/service/user.service';
+
+@Component({
+  selector: 'app-user-login',
+  templateUrl: './user-login.component.html',
+  styleUrls: ['./user-login.component.css']
+})
+export class UserLoginComponent implements OnInit {
+  title: string = 'Login';
+  msg: string = '';
+  user: User = null;
+
+  constructor(private userSvc: UserService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  login() {
+    // call login service using username and password
+    this.userSvc.login(this.user).subscribe(
+      resp => {
+        this.user = resp as User;
+        console.log("Successful login!",this.user);
+        this.router.navigateByUrl('/movie-list');
+      },
+      err => {
+        console.log("User login error!!!",err);
+      }
+    );
+
+
+
+  }
+}
